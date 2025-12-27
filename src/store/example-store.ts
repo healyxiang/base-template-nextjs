4f610dec85f96a7e8306f3c79bf26fcc3499002a
healy <xianglitm@163.com>
2025-12-28 00:24:15 +0800

    chore: add devtool

--- src/store/example-store.ts
+++ src/store/example-store.ts
@@ -1,13 +1,21 @@
-import { create } from 'zustand';
+import { create } from "zustand"
+import { devtools } from "zustand/middleware"
 
 interface ExampleState {
-  count: number;
-  increment: () => void;
-  decrement: () => void;
+  count: number
+  increment: () => void
+  decrement: () => void
 }
 
-export const useExampleStore = create<ExampleState>((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-}));
+export const useExampleStore = create<ExampleState>()(
+  devtools(
+    (set) => ({
+      count: 0,
+      increment: () => set((state) => ({ count: state.count + 1 }), false, "increment"),
+      decrement: () => set((state) => ({ count: state.count - 1 }), false, "decrement"),
+    }),
+    {
+      name: "example-store", // 在 Redux DevTools 中显示的名称
+    }
+  )
+)
